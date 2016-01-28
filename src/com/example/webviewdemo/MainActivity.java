a8aa39254f57b909079bee63a04b40b5fb6b157a
HSW <HSW@HSW-PC>
2016-01-28 17:37:50 +0800

    webview

---
+++ src/com/example/webviewdemo/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.webviewdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.R.string;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+@SuppressLint({ "SetJavaScriptEnabled", "JavascriptInterface" })
+public class MainActivity extends Activity {
+	WebView webView;
+	ProgressBar progressBar;
+	// 获取img标签正则
+	private static final String IMGURL_REG = "<img.*src=(.*?)[^>]*?>";
+	// 获取src路径的正则
+	private static final String IMGSRC_REG = "http:\"?(.*?)(\"|>|\\s+)";
+	// 编码
+	private static final String ECODING = "UTF-8";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initView();
+	}
+
+	private void initView() {
+		// TODO Auto-generated method stub
+		progressBar = (ProgressBar) findViewById(R.id.progress);
+		progressBar.setProgress(50);
+		webView = (WebView) findViewById(R.id.webview);
+		WebSettings settings = webView.getSettings();
+		settings.setJavaScriptEnabled(true);
+		webView.addJavascriptInterface(new InJavaScriptLocalObj(), "local_obj");
+		webView.loadUrl("http://www.sina.com.cn");
+		webView.setWebViewClient(new MyWebViewClient());
+		webView.setWebChromeClient(new WebChromeClient() {
+			@Override
+			public void onProgressChanged(WebView view, int newProgress) {
+				// TODO Auto-generated method stub
+				progressBar.setProgress(newProgress);
+			}
+		});
+	}
+
+	final class MyWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			// TODO Auto-generated method stub
+			view.loadUrl(url);
+			return true;
+		}
+
+		@Override
+		public void onPageStarted(WebView view, String url, Bitmap favicon) {
+			// TODO Auto-generated method stub
+			Log.d("WebView", "onPageStarted");
+			super.onPageStarted(view, url, favicon);
+		}
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			// TODO Auto-generated method stub
+			Log.d("WebView", "onPageFinished ");
+			view.loadUrl("javascript:window.local_obj.showSource('<head>'+"
+					+ "document.getElementsByTagName('html')[0].innerHTML+'</head>');");
+			super.onPageFinished(view, url);
+		}
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			if (webView.canGoBack()) {
+				webView.goBack();
+				return true;
+			} else {
+				System.exit(0);
+			}
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	final class InJavaScriptLocalObj {
+		public void showSource(String html) {
+			// String[] path = getImg1(html);
+			 List<String> list = getImageSrc(getImageUrl(html));
+			 for (int i = 0; i < list.size(); i++) {
+			 Log.e("path", list.get(i)+"");
+			 }
+			
+			
+		}
+	}
+
+	private List<String> getImageUrl(String HTML) {
+		Matcher matcher = Pattern.compile(IMGURL_REG).matcher(HTML);
+		List<String> listImgUrl = new ArrayList<String>();
+		while (matcher.find()) {
+			listImgUrl.add(matcher.group());
+		}
+		return listImgUrl;
+	}
+
+	private List<String> getImageSrc(List<String> listImageUrl) {
+		List<String> listImgSrc = new ArrayList<String>();
+		for (String image : listImageUrl) {
+			Matcher matcher = Pattern.compile(IMGSRC_REG).matcher(image);
+			while (matcher.find()) {
+				listImgSrc.add(matcher.group().substring(0,
+						matcher.group().length() - 1));
+			}
+		}
+		return listImgSrc;
+	}
+}
